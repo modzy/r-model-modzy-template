01203b9b176b295d8528d004f9517c160b08bff5
bmunday3 <bradley.munday@modzy.com>
2021-11-03 20:40:32 -0400

    dev: model.R working inside Python template with rpy2 package, model running inside container, code commented

---
+++ Dockerfile
@@ -0,0 +1,67 @@
+# Please consult the README if you need help in selecting a base image
+FROM rpy2/base-ubuntu:latest
+
+# Create the application working directory
+WORKDIR /opt/app
+
+# Install 
+RUN apt-get update \
+&& apt-get install -y --no-install-recommends \
+    build-essential \
+    ca-certificates \
+    git \
+    make \
+    libssl-dev \
+    zlib1g-dev \
+    libbz2-dev \
+    libreadline-dev \
+    libsqlite3-dev \
+    wget \
+    curl \
+    llvm \
+    libncurses5-dev \
+    xz-utils \
+    tk-dev \
+    libxml2-dev \
+    libxmlsec1-dev \
+    libffi-dev \
+    liblzma-dev \
+    libgl1-mesa-glx \
+&& rm -rf /var/lib/apt/lists/*
+
+# install R packages - modify or add any additional R packages as needed
+RUN R -e "install.packages('jsonlite',dependencies=TRUE, repos='http://cran.rstudio.com/')"  
+
+# install python
+ENV PYTHON_VERSION=3.9.5 \
+    PYTHONUNBUFFERED=1 \
+    PYTHONDONTWRITEBTYECODE=1 \
+    PYENV_ROOT="/.pyenv" \
+    PATH="/.pyenv/bin:/.pyenv/shims:${PATH}"
+RUN git clone --depth=1 https://github.com/pyenv/pyenv.git /.pyenv && \
+    pyenv install ${PYTHON_VERSION} && \
+    pyenv global ${PYTHON_VERSION}
+
+# copy application files into the container image
+# NOTE: to avoid overly large container size, only copy the files actually needed by
+#       explicitly specifying the needed files with the `COPY` command or adjusting
+#       the `.dockerignore` file to ignore unneeded files
+COPY grpc_model ./grpc_model
+COPY model_lib ./model_lib
+COPY asset_bundle/0.1.0 ./asset_bundle/0.1.0
+
+# environment variable to specify model server port
+ENV PSC_MODEL_PORT=45000 \
+    PATH=${PATH}:/home/modzy-user/.local/bin/
+
+ARG CIRCLE_REPOSITORY_URL
+LABEL com.modzy.git.source="${CIRCLE_REPOSITORY_URL}"
+
+# Use pip directly to install Python and gRPC dependencies
+COPY requirements.txt ./
+RUN pip install --no-cache-dir --upgrade pip && \
+    pip install --no-cache-dir -r requirements.txt
+
+# Kick off gRPC server at runtime
+CMD ["python", "-m", "grpc_model.src.model_server"]
+
---
+++ model_lib/src/model.R
@@ -0,0 +1,134 @@
+library(jsonlite)
+# Add additional libraries and packages here as needed
+
+
+# The required output structure for a successful inference run for a models is the following vector:
+
+# output <- c(
+#     "data" <- c(
+#         "result" = result,
+#         "explanation" = explanation,
+#         "drift" = drift
+#     )
+# )
+
+# The parent level `data` value is required and stores a nested vector which represents the output for a specific input. 
+#  The only top-level name within these dictionaries that is required is `result`, however, `explanation` and `drift` are 
+# additional names that may be included if your particular model supports drift detection or explainability. All three of these names
+# (`result`, `explanation`, and `drift`) are required to have a particular format in order to provide platform support.
+# This format type must be specified in the model.yaml file for the version that you are releasing, and the structure for
+# this format type must be followed. If no formats are specified, it is possible to define your own custom structure on a
+# per-model basis.
+# The required output structure for a failed inference run for a models is the following vector:
+
+# errors <- c(
+#     "error_message" = error_messsage
+# )
+
+# Here, all error information that you can extract can be loaded into a single string and returned. This could be a
+# string with a structured error log, or a stack trace dumped to a string.
+
+# Utility function to help format your output correctly before returning the results from your model S4 class  
+get_success_json_structure <- function(inference, explanation, drift) {
+    output_item_list <- c("data" = c("result" = inference, "explanation" = explanation, "drift" = drift)) # nolint
+    return(output_item_list)
+}
+
+# Utility function to help format any errors accumulated during inference before returning the results from your model S4 class
+get_failure_json_structure <- function(error_message) {
+    error_list <- c("error_message" = error_message)
+    return(error_list)
+}
+
+# define model class
+r_model_class <- setRefClass(
+    "MyModelClass",
+    methods = list(
+        initialize = function() {
+            # This constructor should perform all initialization for your model. For example, all one-time tasks such as
+            # loading your model weights into memory should be performed here.
+            # This corresponds to the Status remote procedure call.
+            print("Model loaded!")
+        },
+        preprocess = function(inputs) {
+            # This is a sample data preprocessing method that can optionally be used if any data preprocessing is required 
+            # during inference. If not relevant, the method can be deleted.
+            processed_inputs <- paste(inputs, "plus preprocessing!", sep = " ")
+            return(processed_inputs)
+        },
+        postprocess = function(raw_model_output) {
+            # This is a sample data postprocessing method that can optionally be used if any results postprocessing is required 
+            # during inference. If not relevant, the method can be deleted.
+            postprocessed_outputs <- paste(
+                raw_model_output,
+                "plus postprocessing!",
+                sep = " "
+            )
+            return(postprocessed_outputs)
+        },
+        handle_single_input = function(model_input, detect_drift, explain) {
+            # This method corresponds to the Run remote procedure call for single inputs.
+
+            # model_input will be an R list where the name will be the input filename as defind in model.yaml,
+            # and the value will be decoded data that you are expected to process and pass through your model.
+
+            # You are responsible for processing these files in a manner that is specific to your model, and producing
+            # inference, drift, and explainability results where appropriate.            
+
+            # The following walks through an example of how a user may implement a model inference flow
+
+            # run inference on single input
+            # define name(s) of inputs as defined in model.yaml file
+            input_filename <- "input.txt"
+            input_data <- model_input[[input_filename]]
+
+            # send input data through custom preprocesing or postprocessing
+            # methods, including your model for inference
+            preprocessed_data <- preprocess(input_data)
+            inference_result <- postprocess(preprocessed_data)
+
+            # add custom explainability or drift outputs here as needed if
+            # detect_drift or explain flags are set to TRUE
+            if (!(explain)) {
+                explanation_result <- "None"
+            } else {
+                # add conditional processing here as needed
+                explanation_result <- "None" # change to custom explanations
+            }
+            if (!(detect_drift)) {
+                drift_result <- "None"
+            } else {
+                # add conditional processing here as needed
+                drift_result <- "None" # change to custom explanations
+            }
+
+            # combine inference result, drift result, and explanation result
+            # into a single output object to be converted to json and returned
+            # to user
+            output <- get_success_json_structure(
+                inference_result,
+                explanation_result,
+                drift_result
+            )
+            return(output)
+        },
+        handle_input_batch = function(model_input, detect_drift, explain) {
+            # This is an optional method that will be attempted to be called when more than one inputs to the model
+            # are ready to be processed. This enables a user to provide a more efficient means of handling inputs in batch
+            # that takes advantage of specific properties of their model.
+            # If you are not implementing custom batch processing, this method should return a not implemented error. If you are
+            # implementing custom batch processing, then any unhandled exception will be interpreted as a fatal error that
+            # will result in the entire batch failing. If you would like to allow individual elements of the batch to fail
+            # without failing the entire batch, then you must handle the exception within this function, and ensure the JSON
+            # structure for messages with an error has a top level "error" key with a detailed description of the error
+            # message.
+            # This corresponds to the Run remote procedure call for batch inputs.
+            # c(
+            #     "error" = "your error message here"
+            # )
+            #     
+            
+            simpleError("The batch method is not implemented")
+        }
+    )
+)
